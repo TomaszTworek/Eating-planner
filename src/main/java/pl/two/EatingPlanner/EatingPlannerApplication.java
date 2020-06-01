898d362e705863eb523e17721e6c09a3fdac9ef7
Tomasz Tworek <tomasztworek27@gmail.com>
2020-06-01 18:53:22 +0200

    Initial commit - add H2 configuration

---
+++ src/main/java/pl/two/EatingPlanner/EatingPlannerApplication.java
@@ -0,0 +1,14 @@
+package pl.two.EatingPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatingPlannerApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EatingPlannerApplication.class, args);
+	}
+
+}
