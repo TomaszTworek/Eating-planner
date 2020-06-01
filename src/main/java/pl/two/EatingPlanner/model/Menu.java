1be840138a56f01f5c4d01dbeca108c1f1a27bd5
Tomasz Tworek <tomasztworek27@gmail.com>
2020-06-01 19:51:57 +0200

    #03 Feature - Add Menu entity

--- src/main/java/pl/two/EatingPlanner/model/Menu.java
+++ src/main/java/pl/two/EatingPlanner/model/Menu.java
@@ -1,4 +1,41 @@
 package pl.two.EatingPlanner.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Transactional
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate createdAt;
+
+    @ManyToMany(mappedBy = "menus")
+    @JsonIgnoreProperties
+    private Set<Dish> dishes = new HashSet<>();
+
+    private Byte dishesPerDay;
+
+    private Byte durationTimeInDays;
+
+
 }
+
