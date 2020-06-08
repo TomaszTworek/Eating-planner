555a1f52e6f9d86e70000db4186fdf2a3a2fd067
Tomasz Tworek <tomasztworek27@gmail.com>
2020-06-08 13:25:24 +0200

    #07 Feature - Change database propeties

--- src/main/java/pl/two/EatingPlanner/repository/MenuRepository.java
+++ src/main/java/pl/two/EatingPlanner/repository/MenuRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.two.EatingPlanner.model.Menu;
 
+import java.util.List;
+
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    Menu findByName(String name);
 }
