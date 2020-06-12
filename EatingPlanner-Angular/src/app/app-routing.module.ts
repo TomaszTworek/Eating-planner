b0b8f78605ca3285731ea9c2bfec6a4ef4993809
Tomasz Tworek <tomasztworek27@gmail.com>
2020-06-12 18:30:23 +0200

    #02 Create basic navbar with rounting

--- EatingPlanner-Angular/src/app/app-routing.module.ts
+++ EatingPlanner-Angular/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IngredientComponent} from "./ingredient/ingredient.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'ingredients',
+    component: IngredientComponent
+  },
+  {
+    path:'',
+    component: IngredientComponent,
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing:true})],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- EatingPlanner-Angular/src/app/app.module.ts
+++ EatingPlanner-Angular/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {IngredientComponent} from './ingredient/ingredient.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+//Import forms:
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    IngredientComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
