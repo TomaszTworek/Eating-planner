94ecfa8be5ed9e7d6873690af11bc5806f22691f
Tomasz Tworek <tomasztworek27@gmail.com>
2020-06-16 16:43:27 +0200

    #04 Dish - Created form for dish adding and connect it with backend

--- EatingPlanner-Angular/src/app/app-routing.module.ts
+++ EatingPlanner-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {IngredientComponent} from "./components/ingredient/ingredient.component";
 import {NotFoundComponent} from "./components/not-found/not-found.component";
+import {DishComponent} from "./components/dish/dish.component";
 
 const routes: Routes = [
   {
@@ -9,18 +10,23 @@ const routes: Routes = [
     component: IngredientComponent
   },
   {
-    path:'',
+    path: 'dishes',
+    component: DishComponent
+  },
+  {
+    path: '',
     component: IngredientComponent,
   },
   {
-    path:'**',
-    component:NotFoundComponent
+    path: '**',
+    component: NotFoundComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {enableTracing:true})],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule]
 })
 
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ EatingPlanner-Angular/src/app/components/dish/dish.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import {Ingredient} from "../../models/ingredient";
+import {ApiIngredientService} from "../../services/api-ingredient.service";
+import {IngredientViewModel} from "../ingredient/ingredient.component";
+import {Dish} from "../../models/dish";
+import {ApiDishesService} from "../../services/api-dishes.service";
+import {DishCategory} from "../../models/dish-category.enum";
+
+@Component({
+  selector: 'app-dish',
+  templateUrl: './dish.component.html',
+  styleUrls: ['./dish.component.css']
+})
+export class DishComponent implements OnInit {
+
+  dishes: Dish[] = [];
+
+  model: DishViewModel = {
+    name: '',
+    category: undefined,
+    description: '',
+    calories: undefined
+  }
+
+  constructor(private apiDishesService: ApiDishesService) {
+  }
+
+  ngOnInit(): void {
+    this.getAllDishes();
+  }
+
+  public getAllDishes() {
+    this.apiDishesService.getAllDishes().subscribe(
+      response => {
+        this.dishes= response;
+      },
+      error => {
+        alert("An error occurred while getting all ingredients")
+      }
+    )
+  }
+
+  addDish(): void {
+    this.apiDishesService.addDish(this.model).subscribe(
+      response => {
+        location.reload();
+      },
+      error => {
+        alert("An error has occured while sending feedback");
+      }
+    );
+
+  }
+
+}
+export interface DishViewModel {
+  name: string;
+  category: DishCategory;
+  description: string;
+  calories: number;
+}
