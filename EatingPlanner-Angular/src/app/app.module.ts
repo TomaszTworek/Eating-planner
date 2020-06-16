8fb8432a0cf23cab9cfc349cd9eadceb2e73bc1a
Tomasz Tworek <tomasztworek27@gmail.com>
2020-06-16 09:31:37 +0200

    Small changes on the front

--- EatingPlanner-Angular/src/app/app.module.ts
+++ EatingPlanner-Angular/src/app/app.module.ts
@@ -3,14 +3,14 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {NavigationComponent} from './components/navigation/navigation.component';
 import {IngredientComponent} from './components/ingredient/ingredient.component';
-import {NotFoundComponent} from './components/not-found/not-found.component';
 //Import forms:
 import {FormsModule} from "@angular/forms";
 //Import http to connect with backend
 import {HttpClientModule} from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NavigationComponent} from "./components/navigation/navigation.component";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
 
 @NgModule({
   declarations: [
